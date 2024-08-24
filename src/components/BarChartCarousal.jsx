a4dff0bbd02ea488fcb57cde895351bcc2c5463a
Sinless Rook <142495840+SinlessRook@users.noreply.github.com>
2024-08-24 14:20:06 +0530

    color change

--- src/components/BarChartCarousal.jsx
+++ src/components/BarChartCarousal.jsx
@@ -6,22 +6,24 @@ import { OpeningMostUsedBlack, OpeningMostUsedWhite } from '../Funtions'
 const BarChartCarousal = () => {
   return (
     <>
-    <Paper
-     sx={{
-        padding:'10px',
-        borderRadius: '10px',
-        margin:'10px',
-        width:'100%',
-        textAlign: 'center',
-      }}
-    ><Typography variant="h5" sx={{marginBottom:'10px',padding:'10px'}}>Opening Analysis</Typography>
-    <Typography sx={{marginBottom:'10px',display:'flex',padding:'10px',
-        flexDirection:'row',
-    }}>
-    <BarAnimation dit={OpeningMostUsedWhite} color="White"/>
-    <BarAnimation dit={OpeningMostUsedBlack} color="Black"/>
-    </Typography>
-    </Paper>
+      <Paper
+        sx={{
+          padding: '10px',
+          borderRadius: '10px',
+          margin: '10px',
+          width: '100%',
+          textAlign: 'center',
+          backgroundColor: '#E9ECEF',
+        }}
+      ><Typography variant="h5" sx={{ marginBottom: '10px', padding: '10px' }}>Opening Analysis</Typography>
+        <Typography sx={{
+          marginBottom: '10px', display: 'flex', padding: '10px',
+          flexDirection: 'row',
+        }}>
+          <BarAnimation dit={OpeningMostUsedWhite} color="White" />
+          <BarAnimation dit={OpeningMostUsedBlack} color="Black" />
+        </Typography>
+      </Paper>
     </>
   )
 }
--- src/components/PieChartCarousel.jsx
+++ src/components/PieChartCarousel.jsx
@@ -2,27 +2,28 @@ import React from 'react'
 import PieActiveArc from './pie_chart'
 import { Paper } from '@mui/material'
 const PieChartCarousel = (props) => {
-  const data = props.data  
+  const data = props.data
   return (
     <>
-    <Paper
-            sx={{
-              minWidth:{xs:'100%',sm:'100%',md:'100%',lg:'100%'},
-            margin: '10px',
-              padding: '10px',
-              borderRadius: '10px',
-              textAlign: 'center',
-              color: 'black',
-              fontFamily: 'poppins',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',  
-              justifyContent: 'center' 
-            }}
-        >
-          <h2>Match History</h2>
-          <PieActiveArc data={data} />
-        </Paper>
+      <Paper
+        sx={{
+          minWidth: { xs: '100%', sm: '100%', md: '100%', lg: '100%' },
+          margin: '10px',
+          padding: '10px',
+          borderRadius: '10px',
+          textAlign: 'center',
+          color: 'black',
+          fontFamily: 'poppins',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#DEE2E6'
+        }}
+      >
+        <h2>Match History</h2>
+        <PieActiveArc data={data} />
+      </Paper>
     </>
   )
 }
